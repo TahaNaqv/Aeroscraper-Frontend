c3dcb60e47455baeec6fcfe4169ff73d2355f143
umutvural <umuttvural@gmail.com>
2024-01-11 01:51:09 +0300

    Connect to new api and optimize state updates

--- contracts/app/types.ts
+++ contracts/app/types.ts
@@ -21,3 +21,31 @@ export type CW20TokenInfoResponse = {
     decimals: number;
     total_supply: string;
 }
+
+export interface TotalInfoResponse {
+    page: number
+    perPage: number
+    totalItems: number
+    totalPages: number
+    items: Item[]
+}
+
+export interface Item {
+    ausdInfo: AusdInfo
+    chainName: string
+    collectionId: string
+    collectionName: string
+    created: string
+    id: string
+    totalCollateralAmount: string
+    totalDebtAmount: string
+    totalStake: string
+    updated: string
+}
+
+export interface AusdInfo {
+    decimals: number
+    name: string
+    symbol: string
+    total_supply: string
+}
