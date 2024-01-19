4c8e992bc79499720740d5075719e5d0841811aa
VortexZaa <abidinayhan94@gmail.com>
2024-01-19 19:10:00 +0300

    update

--- app/api/zealy/userInformation/[id]/route.ts
+++ app/api/zealy/userInformation/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-
+export const runtime = "edge";
 export async function GET(req: NextRequest) {
 
   const path = req.nextUrl.pathname;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', trailingSlash: true }
+const nextConfig = { trailingSlash: true,reactStrictMode: false, }
 
 module.exports = nextConfig
