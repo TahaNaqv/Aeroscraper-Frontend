b7c5256c3ec3af68e249306df284102e6578ce05
Nova Ratio <novaratio.tech@gmail.com>
2024-01-31 23:55:57 +0300

    stability pool stake unstake balance check added

--- app/app/dashboard/_components/StabilityPoolModal.tsx
+++ app/app/dashboard/_components/StabilityPoolModal.tsx
@@ -39,8 +39,8 @@ const StabilityPoolModal: FC<Props> = ({ open, onClose, pageData, getPageData })
   const [stakeAmount, setStakeAmount] = useState<number>(0);
   const [unstakeAmount, setUnstakeAmount] = useState<number>(0);
 
-  const stakeDisabled = useMemo(() => stakeAmount <= 0 || stakeAmount > 999, [stakeAmount]);
-  const unstakeDisabled = useMemo(() => unstakeAmount <= 0 || unstakeAmount > 999, [unstakeAmount]);
+  const stakeDisabled = useMemo(() => stakeAmount <= 0 || stakeAmount > 999 || stakeAmount > pageData.ausdBalance, [stakeAmount]);
+  const unstakeDisabled = useMemo(() => unstakeAmount <= 0 || unstakeAmount > 999 || unstakeAmount > pageData.stakedAmount, [unstakeAmount]);
 
   const stakePool = async () => {
     setProcessLoading(true);
